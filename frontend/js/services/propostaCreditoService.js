fa7e9245416ccee8ef23d0236e38e25b0fc6c362
Matheus Tabares <tabares.lopes@gmail.com>
2019-07-08 11:55:20 -0300

    dockerignore, gitignore, frontend dockerizado

--- frontend/js/services/propostaCreditoService.js
+++ frontend/js/services/propostaCreditoService.js
@@ -1,11 +1,11 @@
 angular.module("propostaCredito").factory("propostaCreditoService", function($http) {
     var _cadastrarProposta = function(proposta) {
-        return $http.post("http://localhost:9080/propostas", proposta);
+        return $http.post("http://localhost:8080/propostas", proposta);
     }
 
     var _buscarPorCpf = function(filtroCpf) {
         debugger;
-        return $http.get("http://localhost:9080/propostas/"+filtroCpf);
+        return $http.get("http://localhost:8080/propostas/"+filtroCpf);
     }
 
     return {
